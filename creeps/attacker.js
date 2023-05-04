aa407d629c7bdfa3173d0973180523554a295bb2
FireLord711 <dan.lord@boncormedical.com>
2023-05-04 16:41:02 -0400

    created attacker class

---
+++ creeps/attacker.js
@@ -0,0 +1,50 @@
+//
+//
+var roleBreaker = {
+  /** @param {Creep} creep **/
+  run: function (creep) {
+    // creep.memory.task = "MOVIN";
+    // creep.memory.roomPos = { x: 48, y: 25, roomName: "E9N54" };
+    creep.memory.break = "6453dd062dcf1466c079d6d8";
+
+    if (creep.memory.task != "MOVIN" && creep.memory.task != "BREAK") {
+      roleUtilities.sayState(creep, "IDLE", true);
+      return;
+    }
+
+    // Selected Destination from Memory
+    let breakOBJ = Game.getObjectById(creep.memory.break);
+
+    let breakPos = new RoomPosition(
+      creep.memory.roomPos.x,
+      creep.memory.roomPos.y,
+      creep.memory.roomPos.roomName
+    );
+
+    if (creep.memory.task == "MOVIN") {
+      if (creep.pos.inRangeTo(breakPos, 1)) {
+        creep.memory.task = "BREAK";
+        roleUtilities.sayState(creep, "BREAK", true);
+        this.run(creep);
+      } else {
+        roleUtilities.sayState(creep, "MOVE", true);
+        creep.moveTo(breakPos, {
+          visualizePathStyle: { stroke: "#ffaa00" },
+        });
+      }
+    } else if (creep.memory.task == "BREAK") {
+      let response = creep.dismantle(breakOBJ);
+      if (response == ERR_NOT_IN_RANGE) {
+        creep.moveTo(breakOBJ, {
+          visualizePathStyle: { stroke: "#ffaa00" },
+        });
+      } else if (response == OK) {
+        roleUtilities.sayState(creep, "BREAK", true);
+      } else {
+        roleUtilities.sayState(creep, "ERROR", true);
+      }
+    }
+  },
+};
+
+module.exports.Breaker = roleBreaker;
