ee1c8c3e17650d1b25ebd0b31fd56a19a9c0e7bd
FireLord711 <dan.lord@boncormedical.com>
2023-06-08 00:09:08 -0400

    changed signer to claim and sign,

--- roles/basic/signer.js
+++ roles/basic/signer.js
@@ -3,44 +3,42 @@
 var roleSigner = {
   /** @param {Creep} creep **/
   run: function (creep) {
-    if (creep.memory.atDest) {
+    creepTasks = ["MOVIN", "CLAIM"];
+
+    if (!creepTasks.includes(creep.memory.task)) {
+      creep.memory.task = "MOVIN";
+      return;
+    }
+
+    if (creep.memory.task == "CLAIM") {
       if (creep.room.controller) {
         if (
-          creep.signController(creep.room.controller, "Boop!") ==
+          creep.signController(creep.room.controller, "👽 👾 🤖") ==
           ERR_NOT_IN_RANGE
         ) {
           creep.moveTo(creep.room.controller);
+        } else {
+          creep.claimController(creep.room.controller);
         }
       }
-      // if (creep.room.controller) {
-      //   if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-      //     creep.moveTo(creep.room.controller);
-      //   }
-      // }
+
       //creep.memory.atDest = false;
       //creep.memory.sitPOS = new RoomPosition(33,14,"E45N33");
       this.sing(creep);
-    } else {
-      var mPOS = creep.memory.sitPOS;
-      var sitPOS = new RoomPosition(mPOS.x, mPOS.y, mPOS.roomName);
-      Memory.Foo = mPOS;
-      if (creep.pos.isEqualTo(sitPOS)) {
-        creep.memory.atDest = true;
+    }
+    // MOVIN
+    //
+    else if (creep.memory.task == "MOVIN") {
+      let flagPOS = Game.flags["Flag1"].pos;
+
+      if (creep.pos.inRangeTo(flagPOS, 2)) {
+        creep.memory.task = "CLAIM";
       } else {
-        creep.moveTo(sitPOS, { visualizePathStyle: { stroke: "#ffaa00" } });
+        utils.role.moveTo(creep, flagPOS);
       }
     }
   },
-  body: [MOVE], //CLAIM, CLAIM, MOVE, MOVE],
-  build: function (creepMem) {
-    var newName = "👽 👾 🤖" + Memory.TaskMan.NameNum;
-    Memory.TaskMan.NameNum++;
-    return Game.spawns[creepMem.memory.spawn].spawnCreep(
-      this.body,
-      newName,
-      creepMem
-    );
-  },
+
   sing: function (creep) {
     switch (creep.memory.say) {
       case 1:
@@ -62,11 +60,3 @@ var roleSigner = {
 };
 
 module.exports.Signer = roleSigner;
-
-// Memory.TaskMan.E46N33.spawn.push({
-//   Signer: {
-//     memory: {
-//       role: 'Signer', say: 1, atDest: false, sitPOS: { x: 34, y: 35, roomName: "E45N34" }, spawn: "Vat1"
-//     }
-//   }
-// });
